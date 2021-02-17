5071a5cf6d34d0ac2e04f03a085eb109175b48f5
Pamela Figueroa <ilsepff@gmail.com>
2021-02-16 22:51:38 -0600

    fix async and sycn solutions

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -2,19 +2,21 @@
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
-module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
-    logSources
-      .map((logEntry) => logEntry.pop())
-      .sort((prev, current) => {
-        return prev.date.getTime() - current.date.getTime();
-      })
-      .forEach((logEntry) => {
-        printer.print(logEntry);
-      });
+module.exports = async (logSources, printer) => {
+  const logSourcesPromises = logSources.map(
+    async (logEntry) => await logEntry.popAsync()
+  );
 
-    printer.done();
+  const logSourcesResolved = await Promise.all(logSourcesPromises);
 
-    resolve(console.log("Async sort complete."));
-  });
+  logSourcesResolved
+    .sort((prev, current) => {
+      return prev.date.getTime() - current.date.getTime();
+    })
+    .forEach((logEntry) => {
+      printer.print(logEntry);
+    });
+
+  printer.done();
+  return console.log("Async sort complete.");
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -2,21 +2,19 @@
 
 // Print all entries, across all of the sources, in chronological order.
 
-module.exports = async (logSources, printer) => {
-  const logSourcesPromises = logSources.map(
-    async (logEntry) => await logEntry.popAsync()
-  );
+module.exports = (logSources, printer) => {
+  return new Promise((resolve, reject) => {
+    logSources
+      .map((logEntry) => logEntry.pop())
+      .sort((prev, current) => {
+        return prev.date.getTime() - current.date.getTime();
+      })
+      .forEach((logEntry) => {
+        printer.print(logEntry);
+      });
 
-  const logSourcesResolved = await Promise.all(logSourcesPromises);
+    printer.done();
 
-  logSourcesResolved
-    .sort((prev, current) => {
-      return prev.date.getTime() - current.date.getTime();
-    })
-    .forEach((logEntry) => {
-      printer.print(logEntry);
-    });
-
-  printer.done();
-  return console.log("Sync sort complete.");
+    resolve(console.log("Sync sort complete."));
+  });
 };
